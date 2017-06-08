39c4134d36f37d6808c7b64a7c5ef82c89887b0f
Angular CLI <angular-cli@angular.io>
2017-06-08 09:34:06 -0300

    chore: initial commit from @angular/cli

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { AngularBootstrapPage } from './app.po';
+
+describe('angular-bootstrap App', () => {
+  let page: AngularBootstrapPage;
+
+  beforeEach(() => {
+    page = new AngularBootstrapPage();
+  });
+
+  it('should display welcome message', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('Welcome to app!!');
+  });
+});
